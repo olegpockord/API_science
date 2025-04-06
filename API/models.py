05ac7b6e76e28c6b63edd45ec9c31bf19f38d969
Pockord <oleg222200005555@gmail.com>
2025-04-06 22:32:50 +0300

    Front-end almost done

--- API/models.py
+++ API/models.py
@@ -16,9 +16,9 @@ class TypesOfWork(models.Model):
 
 
 class AuthorNames(models.Model):
-    author_name = models.CharField(max_length=50, null=False, verbose_name='Название')
-    author_last_name = models.CharField(max_length=50, null=False, verbose_name='Название')
-    author_middle_name = models.CharField(max_length=50, null=False, verbose_name='Название')
+    author_name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Имя')
+    author_last_name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Фамилия')
+    author_middle_name = models.CharField(max_length=50, null=True, blank=True, verbose_name='Отчество')
     other_authors = models.TextField(blank=True, null=True, verbose_name='Имена соавторов')
 
     class Meta():
@@ -27,17 +27,20 @@ class AuthorNames(models.Model):
         verbose_name_plural = 'Имена всех авторов'
 
     def __str__(self):
-        return self.pk
+        return f"{self.pk} - {self.author_name}"
 
-
-
+    def full_author_name(self):
+        if self.author_middle_name:
+            return f"{self.author_last_name}., {self.author_name}., {self.author_middle_name}."
+        return f"{self.author_last_name}. {self.author_name}."
+    
 # Create your models here.
 class IntelForScienceWorks(models.Model):
 
     title = models.CharField(max_length=250, null=False, verbose_name='Название')
     annotation = models.TextField(null=True, blank=True, verbose_name='Аннотация')
     OECD = models.CharField(max_length=250, blank=False, null=False, verbose_name='OECD')
-    key_words = models.CharField(max_length=250, blank=True, null=True, verbose_name='Ключевые слова')
+    key_words = models.CharField(max_length=500, blank=True, null=True, verbose_name='Ключевые слова')
     year_publication = models.IntegerField(blank=False, null=False, verbose_name='Год публикации')
     work_type_name = models.CharField(max_length=300, blank=False, null=False, verbose_name='Название источника')
     type_of_work = models.ForeignKey(to=TypesOfWork, on_delete=models.CASCADE, verbose_name='Категория')
--- API/views.py
+++ API/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 
-from API.models import IntelForScienceWorks
+from API.models import AuthorNames, IntelForScienceWorks, TypesOfWork
 
 
 
 def index(request):
 
     information = IntelForScienceWorks.objects.all()
+    authors = AuthorNames.objects.all()
+    types = TypesOfWork.objects.all()
+
 
     context = {
         'information':  information,
+        'authors': authors,
+        'TypesOfWork': types,
     }
 
 
-    return render(request, "index.html")
+
+    return render(request, "index.html", context)
